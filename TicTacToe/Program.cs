ab3266a5a279808896f3b57d94e67530e55dc6df
Josefine <josefineahlstrand@live.se>
2025-04-04 11:04:19 +0200

    added board

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    
+    static char[,] board =
+    {
+        { '1', '2', '3' },
+        { '4', '5', '6' },
+        { '7', '8', '9' },
+    };
+
+    private static int turns = 0;
+
+    static void Main(string[] args)
+    {
+        Field();
+    }
+
+    public static void Field()
+    {
+        Console.Clear();
+        Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[0,0],board[0,1],board[0,2]);
+        Console.WriteLine("-----|-----|-----");
+        Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[1,0],board[1,1],board[1,2]);
+        Console.WriteLine("-----|-----|-----");
+        Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[2,0],board[2,1],board[2,2]);
+        turns++;
+    }
+}
